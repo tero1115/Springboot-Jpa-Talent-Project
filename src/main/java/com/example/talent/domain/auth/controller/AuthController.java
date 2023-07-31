0caca1139466faa17f7eea266390184d23e7ada0
tero1115 <limjisang1115@gmail.com>
2023-07-31 15:26:17 +0900

    로그인/회원가입 화면 구현

---
+++ src/main/java/com/example/talent/domain/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.talent.domain.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/auth/login")
+    public String login() {
+        return "auth/login";
+    }
+
+    @GetMapping("/auth/join")
+    public String join() {
+        return "auth/join";
+    }
+
+    @GetMapping("/auth/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/auth/login";
+    }
+}

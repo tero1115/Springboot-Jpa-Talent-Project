ef0a7d4bcdd13363bb9cd561594e4e10fc4a57d4
tero1115 <limjisang1115@gmail.com>
2023-08-01 11:26:30 +0900

    익셉션핸들러, 응답처리DTO

---
+++ src/main/java/com/example/talent/common/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package com.example.talent.common.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException() {
+        super("잘못된 요청입니다.");
+    }
+}
---
+++ src/main/java/com/example/talent/common/exception/handler/RestExceptionHandler.java
@@ -0,0 +1,60 @@
+package com.example.talent.common.exception.handler;
+
+import com.example.talent.common.dto.ResponseDTO;
+import com.example.talent.common.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<?> handlerBindException(BindException bindException) {
+        HashMap<String, String> errorMap = new HashMap<>();
+
+
+        for (FieldError fieldError : bindException.getBindingResult().getFieldErrors()) {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+
+        }
+
+        return new ResponseEntity<>(
+                ResponseDTO.builder()
+                        .code(1)
+                        .message("요청 데이터를 확인해주세요.")
+                        .data(errorMap)
+                        .build(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    // 에러가 발생했을때 이 메소드를 실행하겠다
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>(
+                ResponseDTO.builder()
+                        .code(1)
+                        .message(exception.getMessage())
+                        .build(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    // 모든 에러를 가져오는 익셉션 핸들러
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>(
+                ResponseDTO.builder()
+                        .code(1)
+                        .message(exception.getMessage())
+                        .build(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

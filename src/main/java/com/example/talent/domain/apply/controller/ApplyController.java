31e1eb7b3c74c1d3641b6bbb2f20691ffbd05ee4
tero1115 <limjisang1115@gmail.com>
2023-08-14 17:54:39 +0900

    신청글 작성하기 구현

--- src/main/java/com/example/talent/domain/apply/controller/ApplyController.java
+++ src/main/java/com/example/talent/domain/apply/controller/ApplyController.java
@@ -1,17 +1,26 @@
 package com.example.talent.domain.apply.controller;
 
+import com.example.talent.domain.post.dto.ResPostTableDTO;
+import com.example.talent.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class ApplyController {
 
-    @GetMapping("/apply/save/{id}")
-    public ModelAndView applySavePage(@PathVariable Integer id) {
+    private final PostService postService;
+
+    @GetMapping("/apply/save/{postId}")
+    public ModelAndView applySavePage(@PathVariable Integer postId) {
 
         ModelAndView modelAndView = new ModelAndView();
+
+        ResPostTableDTO.Post dto = postService.getPostData(postId);
+        modelAndView.addObject("dto", dto);
 
         modelAndView.setViewName("/apply/saveForm");
         return modelAndView;
